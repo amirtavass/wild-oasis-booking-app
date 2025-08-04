36de589e75df08cf83822e95193daeaa7ee31b98
amirtavass <amirtavass62@gmail.com>
2025-08-04 22:02:05 +0330

    learned caching in next

---
+++ app/_components/index.js
@@ -0,0 +1,14 @@
+export { default as Header } from "./Header";
+export { default as Navigation } from "./Navigation";
+export { default as Logo } from "./Logo";
+export { default as CabinList } from "./CabinList";
+export { default as CabinCard } from "./CabinCard";
+export { default as Spinner } from "./Spinner";
+export { default as SpinnerMini } from "./SpinnerMini";
+export { default as SideNavigation } from "./SideNavigation";
+export { default as ReservationCard } from "./ReservationCard";
+export { default as SelectCountry } from "./SelectCountry";
+export { default as SignInButton } from "./SignInButton";
+export { default as SignOutButton } from "./SignOutButton";
+export { default as DeleteReservation } from "./DeleteReservation";
+export { default as Counter } from "./Counter";
