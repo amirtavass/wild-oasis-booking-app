6c31b0e7f463b577568d8c18f9a237c0f5319b7a
amirtavass <amirtavass62@gmail.com>
2025-08-12 17:41:43 +0330

    added more client and server interactions

---
+++ app/_components/Reservation.js
@@ -0,0 +1,18 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+
+async function Reservation({ cabin }) {
+  const [settings, bookedData] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+      <DateSelector cabin={cabin} settings={settings} bookedData={bookedData} />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservation;
